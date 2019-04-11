5fb8b0d8a7fd85751f848cf16fab61a3e7698bbb
KiranGangadhar01 <kiran.gangadhar.01@gmail.com>
2019-04-11 11:51:37 +0530

    BasticUpdation02

--- PCDoctor/computers/models.py
+++ PCDoctor/computers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
