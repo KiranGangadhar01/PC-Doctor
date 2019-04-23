11fe64e82ac190865fbc56e36f267e48ddc58929
KiranGangadhar01 <kiran.gangadhar.01@gmail.com>
2019-04-23 12:02:45 +0530

    Configured PostgreSQL

--- PCDoctor/PCDoctor/settings.py
+++ PCDoctor/PCDoctor/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'PCDoctor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PCDoctor',
+        'USER': 'kiran_postgresql',
+        'PASSWORD': 'qwertyas',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
